2449087544a7182fab9ce503420e5f253edbab3d
kymag20 <kymag20@gmail.com>
2026-06-22 19:33:52 +0800

    Fix portfolio startup and media uploads

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -77,12 +77,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'portfolio' / 'static',
 ]
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
